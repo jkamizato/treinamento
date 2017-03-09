5c696ee9b69893e1debaeabdf22cc5030530b188
Júlio Kamizato <jkamizato@gmail.com>
2017-03-09 16:45:02 -0300

    Send Script

--- modules/custom/mymodule/src/SiteAnnouncementForm.php
+++ modules/custom/mymodule/src/SiteAnnouncementForm.php
@@ -11,15 +11,40 @@ namespace Drupal\mymodule;
 
 use Drupal\Core\Entity\EntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Component\Utility\Unicode;
 
 class SiteAnnouncementForm extends EntityForm {
   public function form(array $form, FormStateInterface $form_state) {
-    return parent::form($form, $form_state);
+    $form = parent::form($form, $form_state);
+    $entity = $this->entity;
+    $form['label'] = [
+      '#type'=> 'textfield',
+      '#title' => 'Label',
+      '#required' => TRUE,
+      '#default_value' => $entity->label(),
+    ];
+
+    $form['message'] = [
+      '#type' => 'textarea',
+      '#title' => t('Message'),
+      '#required' => TRUE,
+      '#default_value' => $entity->getMessage(),
+    ];
+    return $form;
   }
 
   public function save(array $form, FormStateInterface $form_state) {
-    return parent::save($form, $form_state);
+    $entity = $this->entity;
+    $is_new = !$entity->getOriginalId();
+    if ($is_new) {
+      $machine_name = \Drupal::transliteration()->transliterate($entity->label(),
+        LanguageInterface::LANGCODE_DEFAULT, '_');
+      $entity->set('id', Unicode::strtolower($machine_name));
+    }
+
+    $entity->save($form, $form_state);
+
+    //$form_state->setRedirectUrl($this->entity->toUrl('collection'));
   }
-
-
 }
